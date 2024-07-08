a39ac5bcfbe19f5b18fe2672ee5e5317b0edcab9
Boyax123123 <119804792+Boyax123123@users.noreply.github.com>
2024-07-08 10:32:24 +0800

    Add files via upload

---
+++ Final Proj/header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+<!--Header part-->
+    <div id="headBox">
+
+        <div class="headerIconsContainer">
+                <div class="iconBox">
+                    <div class="headIconsCase">
+                        <a href="home.php">
+                            <img src="pics/house.png" class="imgHeadIcons" title="home tab" alt="home page">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="iconBox"> 
+                    <div class="headIconsCase">
+                    <a href="cart.php">
+                        <img src="pics/iconCart.png" class="imgHeadIcons" title="Cart" alt="Cart">
+                    </a>
+                    </div>
+                </div>
+
+                <div class="iconBox">
+                    <div class="headIconsCase">
+                        <a href="cart.php">
+                            <img src="pics/iconWallet.png" class="imgHeadIcons" title="Wallet" alt="Wallet">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="iconBox">
+                    <div class="headIconsCase">
+                        <a href="cart.php">
+                            <img src="pics/iconProfile.png" class="imgHeadIcons" title="Profile" alt="Profile">
+                        </a>
+                    </div>
+                </div>
+                <div class="iconBox">
+
+                <div class="headIconsCase">
+                    <a href="cart.php">
+                        <img src="pics/iconBars.png" class="imgHeadIcons" title="Menu" alt="Menu">
+                    </a>
+                </div>
+                </div>
+              
+            
+        </div>
+ 
+        <!--Header Text  -->
+            
+        <a href="home.php"></a>
+            <div id="headTextContainer">     
+                    <h1><i>Luvier</h1></i>      
+            </div>
+        </a>
+        
+
+     
+    </div>
+   
+    <ul>
+        <li class="headIconsCase">
+            <a href="cart.php">
+                    <img src="pics/iconBars.png" class="imgHeadIcons" title="Menu" alt="Menu">
+                </a>
+        </li>
+        <li class="headIconsCase">
+            <a href="cart.php">
+                    <img src="pics/iconBars.png" class="imgHeadIcons" title="Menu" alt="Menu">
+                </a>
+        </li>
+        <li class="headIconsCase">
+            <a href="cart.php">
+                    <img src="pics/iconBars.png" class="imgHeadIcons" title="Menu" alt="Menu">
+                </a>
+        </li>
+        <li class="headIconsCase">
+            <a href="cart.php">
+                    <img src="pics/iconBars.png" class="imgHeadIcons" title="Menu" alt="Menu">
+                </a>
+        </li>
+
+      
+    </ul>
+</body>
+</html>
---
+++ Final Proj/home.php
@@ -0,0 +1,23 @@
+
+
+
+
+<div id="midBox">
+
+<div id="midSearchPart">
+    <!-- <form id="formBox" >
+        <input type="text" placeholder="Search..." id="searchTextBox" title="Search a product">
+        <button type="submit" id="searchButton" class="icons"> 
+            <img src="pics/iconSearch.png" alt="Search" >
+        </button>
+    </form> -->
+</div>
+   
+
+<div class="midBoxIcons">
+   
+</div>
+
+<br>
+
+</div>
